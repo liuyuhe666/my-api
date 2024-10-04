eb8781ac7e6d5ae7f435e091b4221734ffd7667f
Liu Yuhe <171144077+liuyuhe666@users.noreply.github.com>
2024-10-04 11:02:10 +0800

    feat: add favicon.ico

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { serveStatic } from 'hono/deno'
 import { prettyJSON } from 'hono/pretty-json'
 import { appInfo } from "./constants/index.ts"
 import api from "./routes/index.ts"
 
 const app = new Hono()
 
+app.use('/favicon.ico', serveStatic({ path: './favicon.ico' }))
 app.use(prettyJSON())
 app.use(cors())
 app.notFound((c) => c.json({ ok: false, message: 'Not Found' }, 404))
